787dda308eedd6b179add4b57ab05982c418a833
miguelpel <pelleteratdeborde@yahoo.fr>
2018-10-11 02:33:14 +0300

    corrected the paths on App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './components/Logo.svg';
-import CSS from './components/App.CSS';
+import logo from './Logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {

2308cb0878adf15b9d4680d84b880be62a62eabf
Chreol TRIGNON <chreoltri@gmail.com>
2018-10-11 15:06:39 +0300

    updating, get trouble on App.js Saids that 'Route' is not defined

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const HomePage = () =>
+<div>
+        <h1>HOME PAGE</h1>
+</div>
+
+
+export default HomePage;
---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { LANDING } from '../constants/routes';
+
+
+const LandingPage = () =>
+    <div>
+        <h1>Landing Page</h1>
+
+    </div>
+
+
+
+export default LandingPage;

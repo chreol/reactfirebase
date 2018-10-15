22c77902b0340119c392e11e8af070ddc6bd0ad7
Chreol TRIGNON <chreoltri@gmail.com>
2018-10-15 14:30:57 +0300

    edited

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import SignOutButton from './SignOut';
 import * as routes from '../constants/routes';
 
 const Navigation = () =>
     <div>
         <ul>
             <li><Link to={routes.SIGN_IN}>SIGN_IN</Link></li>
-            <li><Link to={routes.SIGN_UP}>SIGN_UP</Link></li>
+            {/* <li><Link to={routes.SIGN_UP}>SIGN_UP</Link></li> */}
             <li><Link to={routes.LANDING}>LANDING</Link></li>
             <li><Link to={routes.HOME}>HOME</Link></li>
             <li><Link to={routes.ACCOUNT}>ACCOUNT</Link></li>
+            <li><SignOutButton /></li>
         </ul>
     </div>
 
